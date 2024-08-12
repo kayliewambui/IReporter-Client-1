dfa645331e87a1d2843542f0e6d52bf7daaba607
l3xst1ng <howdy.martt@gmail.com>
2024-08-12 09:46:27 +0300

    feat(App): Implement routing and render page components
    - Integrated the `react-router-dom` library to handle client-side routing
    - Defined the top-level routes in the `App.js` file
    - Imported and rendered the appropriate page components based on the defined routes
    - Maintained a clear separation of concerns by delegating page rendering to individual components
    - Utilized the `Navbar` and `Footer` components as common layout elements across the application
    - Ensured the `App.js` file remains focused on the high-level routing and layout structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import Hero from './features/common/components/Hero';
-import HowItWorks from './features/home/components/HowItWorks';
 
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'leaflet/dist/leaflet.css';
+import '@changey/react-leaflet-markercluster/dist/styles.min.css';
+
+// Common components
+import Footer from './features/common/components/Footer';
+
+// Page components
+import Home from './features/pages/components/Home';
+import HowItWorksPage from './features/pages/components/HowItWorksPage';
+import Impact from './features/pages/components/Impact';
+import DataSecurity from './features/pages/components/DataSecurity';
+import About from './features/pages/components/About';
+import Contact from './features/pages/components/Contact';
+import FAQPage from './features/pages/components/FAQPage';
+import PrivacyPolicyPage from './features/pages/components/PrivacyPolicyPage';
 
 function App() {
   return (
-    <div className="App">
-      <Hero />
-      <HowItWorks /> 
-      {/*
-      <FeaturedReports />
-      <Map/>
-      <SecuritySection/>
-      <FAQs/>
-      <AppCredibility/>
-      <Compliance/>
-      <CTA/>
-      <Footer/> */}
-
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/how-it-works" element={<HowItWorksPage />} />
+            <Route path="/impact" element={<Impact />} />
+            <Route path="/data-security" element={<DataSecurity />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/faq" element={<FAQPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
