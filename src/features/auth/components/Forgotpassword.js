d1636d1a8563f3e97ba3d12e55670aac4bc28391
festus-sulumeti <sulumetifestus@outlook.com>
2024-08-18 17:39:00 +0300

    the file

--- src/features/auth/components/Forgotpassword.js
+++ src/features/auth/components/Forgotpassword.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Signup.css';
+import '../../../styling/loggins/Signup.css';
 
 const Forgotpassword = () => {
   const [email, setEmail] = useState('');
