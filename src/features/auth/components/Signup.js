e88e43ca588dd233de5902e96fff3445db3d8807
festus-sulumeti <sulumetifestus@outlook.com>
2024-08-19 00:10:06 +0300

    the file

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -28,7 +28,7 @@ const Signup = () => {
     }
   
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/auth/register', {
+      const response = await fetch('https://ireporter-server-hb42.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestBody),
