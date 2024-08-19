ba0340c739cee4effc74ade54925fa697fc22467
l3xst1ng <howdy.martt@gmail.com>
2024-08-19 14:11:57 +0300

    fix: Implemented fixes

--- src/features/common/components/Navbar.js
+++ src/features/common/components/Navbar.js
@@ -1,30 +1,44 @@
 // components/Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styles from '../../../styling/commons/Navbar.module.css';
-import { Link } from 'react-router-dom';
-
 
 const Navbar = () => {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <span className={styles.logoIcon}>📡</span> IReporter
       </div>
-      <ul className={styles.mainNav}>
-        <li><a href="#home">HOME</a></li>
-        <li><a href="#how-it-works">HOW IT WORKS</a></li>
-        <li><a href="#impact">IMPACT</a></li>
-        <li><a href="#blog">BLOG</a></li>
-        <li><a href="#about">ABOUT</a></li>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul className={`${styles.mainNav} ${isMenuOpen ? styles.active : ''}`}>
+        <li><Link to="/" className={location.pathname === '/' ? styles.active : ''}>HOME</Link></li>
+        <li><Link to="/how-it-works" className={location.pathname === '/how-it-works' ? styles.active : ''}>HOW IT WORKS</Link></li>
+        <li><Link to="/impact" className={location.pathname === '/impact' ? styles.active : ''}>IMPACT</Link></li>
+        <li><Link to="/data-security" className={location.pathname === '/data-security' ? styles.active : ''}>DATA SECURITY</Link></li>
+        <li><Link to="/about" className={location.pathname === '/about' ? styles.active : ''}>ABOUT</Link></li>
+        <li><Link to="/contact" className={location.pathname === '/contact' ? styles.active : ''}>CONTACT</Link></li>
       </ul>
       <div className={styles.userActions}>
-      <Link to={`/login`}>         
-        <button className={styles.loginButton}>LOG IN</button>
-      </Link>
-      <Link to={`/signup`}> 
-        <button className={styles.signupButton}>SIGN UP</button>
-      </Link>
-        <button className={styles.reportNowButton}>REPORT NOW</button>
+        <Link to="/login">
+          <button className={styles.loginButton}>LOG IN</button>
+        </Link>
+        <Link to="/signup">
+          <button className={styles.signupButton}>SIGN UP</button>
+        </Link>
+        <Link to="/report">
+          <button className={styles.reportNowButton}>REPORT NOW</button>
+        </Link>
       </div>
     </nav>
   );
